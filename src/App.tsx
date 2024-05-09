695fa53ed37d980380d38e09c58dfb1778ae5542
Vikas Swain <vikas@Vikass-MacBook-Air.local>
2024-05-10 02:30:36 +0530

    form vaidation

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,34 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center p-3">
-      <form className="gap-4" onSubmit={handleSubmit(onSubmit)}>
+    <div className="flex flex-col justify-center items-center p-3 mt-20 overflow-hidden">
+      <form className="gap-4 border p-5 rounded-lg" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col justify-center items-center p-3 gap-4">
           <input
             {...register("email", {
               required: "Email is required",
-              validate: (value) => value.includes("@"),
+              validate: (value) => {
+                if (!value.includes("@")) {
+                  return("Email must include @")
+                }
+              },
             })}
             type="text"
             placeholder="Email"
-          ></input>
-          {errors.email && <div>{errors.email.message}</div>}
+          />
+          {errors.email && <div className="text-red-500">{errors.email.message}</div>}
           <input
-            {...register("password")}
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 8,
+                message:"Password must be atleast 8 characters"
+              }
+            })}
             type="text"
             placeholder="Password"
-          ></input>
+          />
+          {errors.password && <div className="text-red-500">{errors.password.message}</div>}
           <button type="submit" className="bg-white p-3 hover:bg-slate-500">
             Submit
           </button>
